f305b1c9ddcb05320040f958ce175a6dd1aa3f1f
Bohdan Yanchuk <bogdan.yanchuk@gmail.com>
2022-11-10 15:40:56 +0100

    refactor BaseDAOTests

--- src/test/java/tests/BaseDAOTest.java
+++ src/test/java/tests/BaseDAOTest.java
@@ -15,8 +15,8 @@ import it.eforhum.backoffice.dao.UserGroupsDao;
 import it.eforhum.backoffice.entity.User;
 import it.eforhum.backoffice.entity.UserGroups;
 import it.eforhum.backoffice.enums.Roles;
-import it.eforhum.backoffice.service.GroupService;
-
+import it.eforhum.backoffice.util.DaoFactory;
+import it.eforhum.backoffice.util.ServiceFactory;
 
 public class BaseDAOTest {
 
@@ -27,95 +27,51 @@ public class BaseDAOTest {
 	public void testConnection() {
 		log.info("Test connessione");
 		assertDoesNotThrow(() -> {
-			UserDao.getInstance().testConnessione();
+			DaoFactory.getUserDao().getInstance().testConnessione();
 		});
-	}
-
-	@Test
-	void create() {
-
-		UserGroups userGroups = new UserGroups();
-		User user = new User();
-		userGroups.setCreationUser("test-user");
-		userGroups.setCreationTime(LocalDateTime.now());
-		userGroups.setGroupName("TEST GROUP4");
-		userGroups.setRoles("MODERATOR");
-		userGroups.setPermissions("UPDATE");
-		userGroups.setEnabled(true);
-
-//		assertTrue(userGroups.getId() > 0);
-
-		assertDoesNotThrow(() -> {
-			GroupService.getInstance().createGroup(userGroups);
-		});
-
-		user.setCreationUser("test-user");
-		user.setName("test-name");
-		user.setLastName("test-last-name");
-		user.setLastLogin(LocalDateTime.now());
-		user.setEmail("test-mail3");
-		user.setUsername("test-user1");
-		user.setPassword("test-psw");
-		user.setCreationTime(LocalDateTime.now());
-		user.setUserGroup(userGroups);
-
-//		assertTrue(user.getId() > 0);
-
-		assertDoesNotThrow(() -> {
-			UserService.getInstance().createUser(user);
-		});
-
-		assertDoesNotThrow(() -> {
-			UserDao.getInstance().delete(user);
-		});
-
-		assertDoesNotThrow(() -> {
-			UserGroupsDao.getInstance().delete(userGroups);
-		});
-
 	}
 
 	@Test
 	void createUserAndGroupTest() {
 
-		UserGroups g = new UserGroups();
-		g.setCreationTime(LocalDateTime.now());
-		g.setCreationUser("admin");
-		g.setGroupName("GROUP_USERS");
-		g.setPermissions("READING, RESERVATION");
-		g.setEnabled(true);
-		g.setRoles(Roles.USER.toString());
+		UserGroups userGroup = new UserGroups();
+		userGroup.setCreationTime(LocalDateTime.now());
+		userGroup.setCreationUser("admin");
+		userGroup.setGroupName("GROUP_USERS");
+		userGroup.setPermissions("READING, RESERVATION");
+		userGroup.setEnabled(true);
+		userGroup.setRoles(Roles.USER.toString());
 
-		User u = new User();
-		u.setCreationTime(LocalDateTime.now());
-		u.setCreationUser("java-app");
-		u.setDeleted(false);
-		u.setVerified(true);
-		u.setEmail("hello1@gmail.com");
-		u.setName("Hello");
-		u.setLastLogin(LocalDateTime.now());
-		u.setLastName("World");
-		u.setPassword("qwerty");
-		u.setUsername("1a2b3c");
-		u.setUserGroup(g);
+		User user = new User();
+		user.setCreationTime(LocalDateTime.now());
+		user.setCreationUser("java-app");
+		user.setDeleted(false);
+		user.setVerified(true);
+		user.setEmail("hello1@gmail.com");
+		user.setName("Hello");
+		user.setLastLogin(LocalDateTime.now());
+		user.setLastName("World");
+		user.setPassword("qwerty");
+		user.setUsername("1a2b3c");
+		user.setUserGroup(userGroup);
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().createGroup(g);
+			ServiceFactory.getGroupService().getInstance().createGroup(userGroup);
 		});
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().createUser(u);
+			ServiceFactory.getUserService().getInstance().createUser(user);
 		});
 
-		log.info("User with id {} was created", u.getId());
-		log.info("UserGroup with id {} was created", g.getId());
+		log.info("User with id {} was created", user.getId());
+		log.info("UserGroup with id {} was created", userGroup.getId());
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().deleteUserCompletely(u);
+			ServiceFactory.getUserService().getInstance().deleteUserCompletely(user);
 		});
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().deleteGroup(g);
+			ServiceFactory.getGroupService().getInstance().deleteGroup(userGroup);
 		});
 		log.info("User deleted");
 		log.info("UserGroup deleted");
@@ -125,67 +81,68 @@ public class BaseDAOTest {
 	@Test
 	public void nullIdFindTest() {
 
-		User u = (User) UserDao.getInstance().findById(1000);
+		User user = (User) UserDao.getInstance().findById(1000);
 		log.info("Non existing user search");
-		assertNull(u);
+		assertNull(user);
 
 	}
 
 	@Test
 	public void nullIdFindGroupTest() {
 
-		UserGroups u = (UserGroups) UserGroupsDao.getInstance().findById(1000);
+		UserGroups user = (UserGroups) UserGroupsDao.getInstance().findById(1000);
 		log.info("Non existing group search");
-		assertNull(u);
+		assertNull(user);
 
 	}
 
 	@Test
 	public void notNullFindUserAndGroupTest() {
 
-		UserGroups g = new UserGroups();
-		g.setCreationTime(LocalDateTime.now());
-		g.setCreationUser("admin");
-		g.setGroupName("GROUP_USERS");
-		g.setPermissions("READING, RESERVATION");
-		g.setEnabled(true);
-		g.setRoles(Roles.USER.toString());
+		UserGroups userGroup = new UserGroups();
+		userGroup.setCreationTime(LocalDateTime.now());
+		userGroup.setCreationUser("admin");
+		userGroup.setGroupName("GROUP_USERS");
+		userGroup.setPermissions("READING, RESERVATION");
+		userGroup.setEnabled(true);
+		userGroup.setRoles(Roles.USER.toString());
 
-		User u = new User();
-		u.setCreationTime(LocalDateTime.now());
-		u.setCreationUser("java-app");
-		u.setDeleted(false);
-		u.setVerified(true);
-		u.setEmail("hell555@gmail.com");
-		u.setName("Hello");
-		u.setLastLogin(LocalDateTime.now());
-		u.setLastName("World");
-		u.setPassword("qwerty");
-		u.setUsername("1a2b3c");
-		u.setUserGroup(g);
+		User user = new User();
+		user.setCreationTime(LocalDateTime.now());
+		user.setCreationUser("java-app");
+		user.setDeleted(false);
+		user.setVerified(true);
+		user.setEmail("hell555@gmail.com");
+		user.setName("Hello");
+		user.setLastLogin(LocalDateTime.now());
+		user.setLastName("World");
+		user.setPassword("qwerty");
+		user.setUsername("1a2b3c");
+		user.setUserGroup(userGroup);
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().createGroup(g);
+			ServiceFactory.getGroupService().getInstance().createGroup(userGroup);
 		});
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().createUser(u);
+			ServiceFactory.getUserService().getInstance().createUser(user);
 		});
 
-		log.info("User with id {} was created", u.getId());
-		log.info("UserGroup with id {} was created", g.getId());
+		log.info("User with id {} was created", user.getId());
+		log.info("UserGroup with id {} was created", userGroup.getId());
 
-		assertNotNull(UserDao.getInstance().findById(u.getId()));
-		assertNotNull(UserGroupsDao.getInstance().findById(g.getId()));
-		log.info("User found is {}", UserDao.getInstance().findById(u.getId()));
-		log.info("Group found is {}", UserGroupsDao.getInstance().findById(g.getId()));
+		assertNotNull(DaoFactory.getUserDao().getInstance().findById(user.getId()));
+		assertNotNull(DaoFactory.getUserGroupDao().getInstance().findById(userGroup.getId()));
+
+		log.info("User found is {}", DaoFactory.getUserDao().getInstance().findById(user.getId()));
+		log.info("Group found is {}", DaoFactory.getUserGroupDao().getInstance().findById(userGroup.getId()));
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().deleteUserCompletely(u);
+			ServiceFactory.getUserService().getInstance().deleteUserCompletely(user);
 		});
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().deleteGroup(g);
+			ServiceFactory.getGroupService().getInstance().deleteGroup(userGroup);
 		});
 		log.info("User deleted");
 		log.info("UserGroup deleted");
@@ -195,84 +152,84 @@ public class BaseDAOTest {
 	@Test
 	public void updateUserTest() {
 
-		User u = new User();
-		u.setCreationTime(LocalDateTime.now());
-		u.setCreationUser("java-app");
-		u.setDeleted(false);
-		u.setVerified(true);
-		u.setEmail("hello45555@gmail.com");
-		u.setName("Hello");
-		u.setLastLogin(LocalDateTime.now());
-		u.setLastName("World");
-		u.setPassword("qwerty");
-		u.setUsername("1a2b3c");
+		User user = new User();
+		user.setCreationTime(LocalDateTime.now());
+		user.setCreationUser("java-app");
+		user.setDeleted(false);
+		user.setVerified(true);
+		user.setEmail("hello45555@gmail.com");
+		user.setName("Hello");
+		user.setLastLogin(LocalDateTime.now());
+		user.setLastName("World");
+		user.setPassword("qwerty");
+		user.setUsername("1a2b3c");
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().createUser(u);
+			ServiceFactory.getUserService().getInstance().createUser(user);
 		});
 
-		log.info("User created is {} ", u);
+		log.info("User created is {} ", user);
 
-		User u2 = new User();
-//		u2.setUpdateTime(LocalDateTime.now());
-//		u2.setUpdateUser("java-app");
-
-//		u2.setDeleted(false);
-//		u2.setVerified(true);
-		u2.setEmail("theHelloWord11@gmail.com");
-		u2.setName("Pryvit");
-		u2.setLastLogin(LocalDateTime.now());
-		u2.setLastName("Svit");
-		u2.setPassword("qwerty");
-		u2.setUsername("1a2b3c");
+		User updatedUser = new User();
+		updatedUser.setUpdateTime(LocalDateTime.now());
+		updatedUser.setUpdateUser("java-app");
+		updatedUser.setDeleted(false);
+		updatedUser.setVerified(true);
+		updatedUser.setEmail("theHelloWord11@gmail.com");
+		updatedUser.setName("Pryvit");
+		updatedUser.setLastLogin(LocalDateTime.now());
+		updatedUser.setLastName("Svit");
+		updatedUser.setPassword("qwerty");
+		updatedUser.setUsername("1a2b3c");
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().updateUser(u.getId(), u2);
+			ServiceFactory.getUserService().getInstance().updateUser(user.getId(), updatedUser);
 		});
-		log.info("Updated user is {} ", BackofficeService.getInstance().findByIdUser(u.getId()));
+		log.info("Updated user is {} ", ServiceFactory.getUserService().getInstance().findByIdUser(user.getId()));
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().deleteUserCompletely(u);
+			ServiceFactory.getUserService().getInstance().deleteUserCompletely(user);
 		});
 
 	}
 
 	public void updateGroupTest() {
-		UserGroups g = new UserGroups();
-		g.setCreationTime(LocalDateTime.now());
-		g.setCreationUser("admin");
-		g.setGroupName("GROUP_USERS");
-		g.setPermissions("READING, RESERVATION");
-		g.setEnabled(true);
-		g.setRoles("USER");
+		UserGroups userGroup = new UserGroups();
+		userGroup.setCreationTime(LocalDateTime.now());
+		userGroup.setCreationUser("admin");
+		userGroup.setGroupName("GROUP_USERS");
+		userGroup.setPermissions("READING, RESERVATION");
+		userGroup.setEnabled(true);
+		userGroup.setRoles("USER");
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().createGroup(g);
+			ServiceFactory.getGroupService().getInstance().createGroup(userGroup);
 		});
 
-		UserGroups g2 = new UserGroups();
-		g2.setCreationTime(LocalDateTime.now());
-		g2.setCreationUser("admin");
-		g2.setGroupName("GROUP_USERS_TYPE2");
-		g2.setPermissions("READING, RESERVATION, EDITING");
-		g2.setEnabled(true);
-		g2.setRoles(Roles.ROLE_UPDATE_SLOT.toString());
+		UserGroups updatedGroup = new UserGroups();
+		updatedGroup.setCreationTime(LocalDateTime.now());
+		updatedGroup.setCreationUser("admin");
+		updatedGroup.setGroupName("GROUP_USERS_TYPE2");
+		updatedGroup.setPermissions("READING, RESERVATION, EDITING");
+		updatedGroup.setEnabled(true);
+		updatedGroup.setRoles(Roles.ROLE_UPDATE_SLOT.toString());
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().updateGroup(g.getId(), g2);
+			ServiceFactory.getGroupService().getInstance().updateGroup(userGroup.getId(), updatedGroup);
 		});
 
-		log.info("Group has been updated to {} ", BackofficeService.getInstance().findByIdGroup(g.getId()));
+		log.info("Group has been updated to {} ",
+				ServiceFactory.getGroupService().getInstance().findByIdGroup(userGroup.getId()));
 
 		assertDoesNotThrow(() -> {
-			BackofficeService.getInstance().deleteGroup(g);
+			ServiceFactory.getGroupService().getInstance().deleteGroup(userGroup);
 		});
 	}
 
 	@Test
 	void addUserToGroupTest() {
-		User user = assertDoesNotThrow(() -> BackofficeService.getInstance().findByIdUser(3));
-		UserGroups userGroup = assertDoesNotThrow(() -> BackofficeService.getInstance().findByIdGroup(15)); //15 = default user group
+		User user = assertDoesNotThrow(() -> ServiceFactory.getUserService().getInstance().findByIdUser(1));
+		UserGroups userGroup = assertDoesNotThrow(() -> ServiceFactory.getGroupService().getInstance().findByIdGroup(15)); //15 = default user group
 		
 		assertNotNull(user);
 		assertNotNull(userGroup);
@@ -282,7 +239,7 @@ public class BaseDAOTest {
 		
 		
 		log.info("Attempting to add user {} to group {}", user.getUsername(), userGroup.getGroupName());
-		assertDoesNotThrow(() -> BackofficeService.getInstance().addUserToGroup(userGroup, user));
+		assertDoesNotThrow(() -> ServiceFactory.getUserService().getInstance().addUserToGroup(userGroup, user));
 		
 		log.info("Succesfully added user {} to group {}", user.getUsername(), userGroup.getGroupName());
 	}
